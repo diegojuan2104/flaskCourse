e54538debcc896c92745ce946d73372ded4801d8
diegojuan2104 <diegojuan2104@gmail.com>
2020-07-04 16:28:04 -0500

    sesion 7

---
+++ 5_Flask_SQLAlchemy/code/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+api = Api(app)
+app.secret_key = '123456789123456789qwewqqwer'
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWT(app, authenticate, identity)
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister,'/register')
+
+# allows only run in app.py
+if __name__ == '__main__':
+    from db import db 
+    db.init_app(app)
+    app.run(port = 3000, debug=True)
